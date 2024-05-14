7c75e8d895b1bb46d5be7bc775bbf8020910ecd7
Quang Le <leanhquang2803@gmail.com>
2024-05-14 21:59:17 +0200

    Complete Like feature in DiscoveryScreen and add authorName field for Post items

--- app/src/main/java/hu/ait/tastebuddies/ui/screen/discovery/DiscoveryViewModel.kt
+++ app/src/main/java/hu/ait/tastebuddies/ui/screen/discovery/DiscoveryViewModel.kt
@@ -8,41 +8,52 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
+import hu.ait.tastebuddies.data.DataManager
 import hu.ait.tastebuddies.data.Post
 import hu.ait.tastebuddies.data.PostWithId
+import hu.ait.tastebuddies.data.User
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 
 class DiscoveryViewModel: ViewModel() {
-    var messagesUiState: DiscoveryUIState by mutableStateOf(DiscoveryUIState.Init)
-    val db = Firebase.firestore
+    private var likesList: List<String> = emptyList()
+    private val db = Firebase.firestore
 
-    fun getLikes(userID: String = "", authorID: String): List<String>? {
-        val docRef = db.collection("posts").document(authorID)
-        var likesList: List<String> = emptyList()
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    likesList = document.data?.get("likes") as List<String>
+    fun getLikes(postID: String) = callbackFlow {
+        val snapshotListener =
+            FirebaseFirestore.getInstance().collection("posts").document(postID)
+                .addSnapshotListener() { snapshot, e ->
+                    val response = if (snapshot != null) {
+                        val post = snapshot.toObject(Post::class.java)
+                        likesList = post!!.likes
+                    } else {
+
+                    }
+
+                    trySend(response) // emit this value through the flow
                 }
-            }
-            .addOnFailureListener { exception ->
-                Log.d("GET LIKES", "get failed with ", exception)
-            }
-        return if (userID in likesList) {
-            null
-        } else {
-            likesList
+        awaitClose { // when we navigate out from the screen,
+            // the flow stosp and we stop here the firebase listener
+            snapshotListener.remove()
         }
     }
 
-    fun incrementLikes(userID: String = "", authorID: String) {
-        val likesList = getLikes(userID, authorID)?.toMutableList()
-        val docRef = db.collection("posts").document(authorID)
+    fun updateLikes(postID: String, likes: List<String>) {
+        val mutableLikesList = likes.toMutableList()
+        if (mutableLikesList.contains(DataManager.email)) {
+            Log.d("TEST", "email in like list")
+            mutableLikesList.remove(DataManager.email)
+        } else {
+            mutableLikesList.add(DataManager.email)
+        }
+        val docRef = db.collection("posts").document(postID)
         docRef
-            .update("likes", likesList?.toList())
+            .update("likes", mutableLikesList.toList())
             .addOnSuccessListener { Log.d("INCREMENT LIKES", "DocumentSnapshot successfully updated!") }
             .addOnFailureListener { e -> Log.w("INCREMENT LIKES", "Error updating document", e) }
+
     }
 
     fun postsList() = callbackFlow {
@@ -84,4 +95,9 @@ sealed interface DiscoveryUIState {
     object Loading : DiscoveryUIState
     data class Success(val postList: List<PostWithId>) : DiscoveryUIState
     data class Error(val error: String?) : DiscoveryUIState
+}
+
+sealed interface LikesUiState {
+    object Init : LikesUiState
+    data class Success(val likesList: List<String>): LikesUiState
 }
