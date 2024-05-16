e898cc698186a17592e01b3330f7eb6e2cd46c83
thanhdang2712 <danghuyenthanh2003@gmail.com>
2024-05-16 02:33:19 +0200

    Finished all features: Search Dialogue, Lazy Grid, Star important food feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.storage.ktx)
     implementation(libs.firebase.firestore)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
