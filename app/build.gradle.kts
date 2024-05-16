058c4c3c36a4e1fdd5f12b2758e02fd26ca26f2d
Quang Le <leanhquang2803@gmail.com>
2024-05-16 13:49:36 +0200

    Add Firebase Storage plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.firebase.storage.ktx)
     implementation(libs.firebase.firestore)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
